eb5be4f956e3dc52b4930c5f983779ed65db09d1
= <=>
2024-05-07 14:00:06 +0700

    edit layout

---
+++ lib/pages/dialog_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class DialogPage extends StatelessWidget {
+  const DialogPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Cara pengambilan gambar"),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/Bunga Anyelir.jpg',
+              width: 200,
+              height: 200,
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "This is the main image",
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: (){
+              Navigator.of(context).pop();
+            }, child: const Text("Got it"))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flower_scan/pages/dialog_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flower Scan"),
+        actions: [
+          IconButton(
+            onPressed: (){
+            showDialog(context: context, 
+            builder: (BuildContext context){
+              return const DialogPage();
+            });
+          }, icon: const Icon(Icons.info))
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //Placeholder for image
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(onPressed: (){
+
+              }, 
+              child: const Text("Camera")),
+              ElevatedButton(onPressed: (){
+
+              },
+              child: const Text("Upload Photo"))
+            ],
+          ),
+          const SizedBox(height: 20),
+
+          ElevatedButton(onPressed: (){
+
+          },
+          child: const Text("Predict "))
+        ],
+      ),
+    );
+  }
+}
