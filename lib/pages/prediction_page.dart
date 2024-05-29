e9ea33b5b58f93fbd6f217b6f5f03854ac17c2d2
= <=>
2024-05-29 22:17:55 +0700

    add prediction + add link GCP

---
+++ lib/pages/prediction_page.dart
@@ -0,0 +1,67 @@
+// import 'dart:io';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+
+// class PredictionService {
+//   static const String apiUrl = 'http://127.0.0.1:5000/Upload';
+
+//   static Future<String?> predictImage(File image) async {
+//     final request = http.MultipartRequest(
+//       'POST',
+//       Uri.parse(apiUrl),
+//     );
+//     request.files.add(await http.MultipartFile.fromPath('file', image.path));
+//     final response = await request.send();
+
+//     if (response.statusCode == 200) {
+//       final res = await http.Response.fromStream(response);
+//       final data = json.decode(res.body);
+//       return data['prediction'];
+//     } else {
+//       print('Failed to predict image.');
+//       return null;
+//     }
+//   }
+// }
+
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class PredictionService {
+  static const String localApiUrl = 'http://192.168.127.6:5000/Upload';
+
+  // Link URL :
+  // Wifi (JTI-Polinema) :  http://192.168.56.17:5000 
+  // LAN Rumah : 172.26.64.1
+
+  static const String gcpApiUrl = 'https://flowers-api-do6lrlvoia-et.a.run.app/Upload'; // Replace with actual GCP URL
+
+  static Future<Map<String, dynamic>?> predictImage(File image, {bool useLocal = false}) async {
+    final String apiUrl = useLocal ? localApiUrl : gcpApiUrl;
+
+    try {
+      final request = http.MultipartRequest(
+        'POST',
+        Uri.parse(apiUrl),
+      );
+      request.files.add(await http.MultipartFile.fromPath('file', image.path));
+      final response = await request.send();
+
+      if (response.statusCode == 200) {
+        final res = await http.Response.fromStream(response);
+        final data = json.decode(res.body);
+        return {
+          'accuracy': data['accuracy'],
+          'classificationResult': data['classification result'],
+        };
+      } else {
+        print('Failed to predict image with status code: ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      print('Failed to predict image with error: $e');
+      return null;
+    }
+  }
+}
