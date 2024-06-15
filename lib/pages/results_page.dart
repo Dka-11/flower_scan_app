f931df3197808d662d1cb0780ce1caa17fa7549c
= <=>
2024-06-15 10:42:55 +0700

    changes to list

--- lib/pages/results_page.dart
+++ lib/pages/results_page.dart
@@ -4,18 +4,29 @@ import 'package:flutter/material.dart';
 
 class PredictionResultScreen extends StatelessWidget {
   final File image;
-  final String classificationResult;
-  final String accuracy;
+  final Map listOfData;
+  // final String classificationResult;
+  // final String accuracy;
 
   PredictionResultScreen({
     required this.image,
-    required this.classificationResult,
-    required this.accuracy,
+    required this.listOfData,
+    // required this.classificationResult,
+    // required this.accuracy,
   });
 
   @override
   Widget build(BuildContext context) {
-    var accuracyInNumber = double.parse(accuracy) * 100;
+    String? classificationResult, errorMessage;
+    double? accuracyInNumber;
+
+    if (listOfData['code'] == 200) {
+      classificationResult = listOfData['classificationResult'];
+      accuracyInNumber = double.parse(listOfData['accuracy']) * 100;
+    } else {
+      errorMessage = listOfData['error'];
+    }
+    
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green[400],
@@ -27,24 +38,33 @@ class PredictionResultScreen extends StatelessWidget {
           children: [
             Center(
             child:
-            Padding(
-              padding: const EdgeInsets.only(bottom: 36),
-              child: Image.file(
-                    image,
-                    height: MediaQuery.of(context).size.height/1.7
-                  ),
-            ),
-          ),
-            Text(
-              'Hasil Klasifikasi : $classificationResult',
-              style: TextStyle(fontSize: 20),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Akurasi: $accuracyInNumber%',
-              style: TextStyle(fontSize: 20),
-              textAlign: TextAlign.center,
+              Padding(
+                padding: const EdgeInsets.only(bottom: 36),
+                child: Image.file(
+                      image,
+                      height: MediaQuery.of(context).size.height/1.7
+                    ),
+              ),
+            ), errorMessage != null
+            ? Text(
+                'Terjadi error : $errorMessage',
+                style: const TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              )
+            : Column(
+              children: [
+                Text(
+                  'Hasil Klasifikasi : $classificationResult',
+                  style: const TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Akurasi: $accuracyInNumber%',
+                  style: const TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
           ],
         ),
